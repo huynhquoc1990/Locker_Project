7dc042ec08504896b37fadad531ca5eb5b861c67
Quoc <huynhquoc1990@gmail.com>
2021-04-25 06:12:32 +0700

    0.0.1

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(
+    name='Locker_Project',
+    version='0.0.1',
+    packages=['Locker_Project'],
+    url='https://github.com/huynhquoc1990/Locker_Project.git',
+    license='MIT',
+    author='Huỳnh Tấn Quốc',
+    author_email='huynhquoc1990@gamil.com',
+    description='Locker_Project Load',
+    #install_requires=["",""],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description
+)
